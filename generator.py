a3e9d6a167a587e093a0d3ae88beda9cc1dd2533
catzoo <catzoo7007@gmail.com>
2022-02-11 19:10:45 -0600

    Added in support for decimals
    - Added in configuration for stepping. This will allow someone to generate decimals

--- generator.py
+++ generator.py
@@ -2,11 +2,25 @@ from pathlib import Path
 import os
 import sqlite3
 
+def custom_range(a, b, step):
+    # Same as range, only this supports decimal steps
+    # Note: This may have a rounding error, see here for more info -
+    # https://stackoverflow.com/questions/477486/how-to-use-a-decimal-range-step-value
+    i = a
+    # Grabbing how many decimal places step is
+    round_to = len(str(step)[2:])
+
+    while i < b:
+        yield i
+        i = round(i + step, round_to)
+
+
 DB_FILE = "calc.db"
 while True:
     try:
         min_num = int(input("Choose the minimum number to generate: "))
         max_num = int(input("Choose the max number to generate: "))
+        step_num = float(input("Choose the number to step by: "))
     except ValueError:
         print("Please enter a number.")
     else:
@@ -19,14 +33,14 @@ con = sqlite3.connect(DB_FILE)
 cursor = con.cursor()
 cursor.execute("CREATE TABLE math ("
                "rowID INTEGER PRIMARY KEY,"
-               "a INTEGER NOT NULL, "
+               "a REAL NOT NULL, "
                "symbol TEXT NOT NULL, "
-               "b INTEGER NOT NULL, "
+               "b REAL NOT NULL, "
                "answer TEXT NOT NULL"  # Setting answer as a text so we can give 0 / 0 = undefined
                ")")
 for symbol in ["+", "-", "/", "*"]:
-    for a in range(min_num, max_num + 1):
-        for b in range(min_num, max_num + 1):
+    for a in custom_range(min_num, max_num + 1, step_num):
+        for b in custom_range(min_num, max_num + 1, step_num):
             try:
                 answer = eval(f"{a}{symbol}{b}")
             except ZeroDivisionError:
@@ -35,7 +49,6 @@ for symbol in ["+", "-", "/", "*"]:
             cursor.execute(f"INSERT INTO math "
                            f"(a, symbol, b, answer) VALUES ({a}, \"{symbol}\", {b}, \"{answer}\")"
                            )
-
         print(f"({a} / {max_num}) for {symbol} ", end="\r")
     con.commit()
     print("")  # adding next line
